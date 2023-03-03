1f2cab4e594ffbaabad0a947f0aaa27e39011f16
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-03-03 14:15:36 +0300

    Started solution

---
+++ Python/Pick peaks/solution.py
@@ -0,0 +1,17 @@
+def pick_peaks(arr):
+    result = {
+        "pos": [],
+        "peaks": []
+    }
+    if not arr: return result
+
+    for 
+
+    return result
+
+if __name__ == "__main__":
+    # print(pick_peaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]))
+    # print(pick_peaks([0, 1, 2, 5, 1, 0]))
+    print(pick_peaks([3,2,3,6,4,1,2,3,2,1,2,3]))
+    # print(pick_peaks([3,2,3,6,4,1,2,3,2,1,2,2,2,1]))
+    # print(pick_peaks([1, 2, 2, 2, 1]))
