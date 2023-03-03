6202421e5d31b827a563c32dcc3f6a5283dadb6b
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-03-03 19:36:59 +0300

    Created two variants of algorithm

--- Python/Van Eck's Sequence/solution.py
+++ Python/Van Eck's Sequence/solution.py
@@ -1,12 +1,35 @@
+
 def seq(n):
-    result = [0, 0]
-    increment = 0
-    for i in range(2, n + 1):
-        if increment == result[i - 2]:
-            increment += 1
-            result.append(increment)
-    return result
+    r = [0]
+    last_indexes = dict()
+    for i in range(1, n):
+        if r[i - 1] in last_indexes:
+            r.append(i - 1 - last_indexes[r[i - 1]])
+        else:
+            r.append(0)
+        last_indexes[r[i - 1]] = i - 1
+    return r
+
+
+def find(arr: list, term: int) -> int:
+    m_i = 0
+    for i in range(len(arr)):
+        if arr[i] == term and i > m_i:
+            m_i = i
+
+    return m_i
+
+def seq2(n):
+    r = [0]
+    for i in range(1, n + 1):
+        if r[i - 1] in r[:-1]:
+            r.append(i - 1 - find(r[:-1], r[i - 1]))
+        else:
+            r.append(0)
+    return r
+
 
 
 if __name__ == "__main__":
-    print(seq(5))
+    print(seq2(20))
+    # print(find([0, 0, 1, 0, 2, 0, 2], 1))
