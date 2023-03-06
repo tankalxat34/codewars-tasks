51b22176a8d4f1f3523a6333d3a41fcb205f7bc0
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-03-06 16:39:54 +0300

    Done Invalid solution

--- Python/Sum Strings as Numbers/solution.py
+++ Python/Sum Strings as Numbers/solution.py
@@ -1,48 +1,45 @@
 
 def sum_strings(x: str, y: str) -> str:
     """
-    1235213572135974534 + 623242142134325
+    1
+    62
+    49
+    --
+   111
     
-    111   <-- in mind  
-    9999
-    0001
-    ----
-   10000
-
-      1    
-    1239
-    0005
-    ----
-    1244      
-
     """
+    print(x, y)
+
+    if not x: return "0"
+    if not y: return "0"
+
     result = str()
 
-    a, b = "0" * (len(x) - 1) + y, "0" * (len(y) - 1) + x
+
+    a, b = "0" * (len(x) - len(y)) + y, "0" * (len(y) - len(x)) + x
 
     print(a, b)
 
-    in_mind = 0
-
+    in_mind = "0"
     for i in range(1, len(a) + 1):
-        local_a, local_b = int(a[-i]) + int(in_mind), int(b[-i])
-        
-        in_mind = str(local_a + local_b)[0]
-        
-        print(f"{i = }", f"{in_mind}")
+        summ = int(in_mind) + int(a[-i]) + int(b[-i])
 
-        try:
-            result += str(local_a + local_b)[1]
-        except IndexError:
-            result += in_mind
+        if summ > 9:
+            print(i, len(a), summ)
+            if not (i == len(a)):
+                in_mind = str(summ)[0]
+                result += str(summ)[1]
+            else:
+                in_mind = "0"
+                result += str(summ)[::-1]
+        else:
+            in_mind = "0"
+            result += str(summ)
+
 
     return result[::-1]
 
 
 
 if __name__ == "__main__":
-    print(sum_strings('5', '1239')) # 1244
-    # print(sum_strings('9999', '5')) # 1244
-    # print(sum_strings('1', '1')) # 1244
-    # print(sum_strings('1234', '2')) # 3
-
+    print(sum_strings("00103", "08567"))
