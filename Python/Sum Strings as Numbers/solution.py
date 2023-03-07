2794a2fa0d1312d18fb8b3c3152cd8b39182609e
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-03-07 11:01:53 +0300

    completed task sum strings

--- Python/Sum Strings as Numbers/solution.py
+++ Python/Sum Strings as Numbers/solution.py
@@ -1,45 +1,40 @@
 
 def sum_strings(x: str, y: str) -> str:
-    """
-    1
-    62
-    49
-    --
-   111
-    
-    """
-    print(x, y)
+    result = ""
 
-    if not x: return "0"
-    if not y: return "0"
-
-    result = str()
-
-
-    a, b = "0" * (len(x) - len(y)) + y, "0" * (len(y) - len(x)) + x
-
-    print(a, b)
+    _x = "0" * (len(y) - len(x)) + x
+    _y = "0" * (len(x) - len(y)) + y
 
     in_mind = "0"
-    for i in range(1, len(a) + 1):
-        summ = int(in_mind) + int(a[-i]) + int(b[-i])
+    for i in range(1, len(_x) + 1):
+        local_x, local_y = int(_x[-i]) + int(in_mind), int(_y[-i])
+        summ = local_x + local_y
 
         if summ > 9:
-            print(i, len(a), summ)
-            if not (i == len(a)):
+            if i != len(_x):
                 in_mind = str(summ)[0]
                 result += str(summ)[1]
             else:
-                in_mind = "0"
-                result += str(summ)[::-1]
+                in_mind = str(summ)
+                result += in_mind[::-1]
+                return result[::-1]
         else:
             in_mind = "0"
             result += str(summ)
 
+    result = result[::-1]
 
-    return result[::-1]
+    for i in range(len(result)):
+        if result[i] == "0":
+            result = result[i + 1:]
+        else:
+            break
 
+    return result or "0"
 
 
 if __name__ == "__main__":
     print(sum_strings("00103", "08567"))
+    # print(rmzeros("08670"))
+    
+    # print(sum_strings("123", "456"))
